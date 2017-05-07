2f5fef6fe25e64f285b842720a6a2ff66b54655e
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-07 15:23:22 +0200

    add target start tests, more macros, add a note for testing

--- src/tests.rs
+++ src/tests.rs
@@ -1,16 +1,44 @@
+/*
+** Tests - test with 'cargo run -- --no-capture --test-threads 1'
+*/
+
 use std::fs::{File, remove_file};
 use std::path::Path;
 use super::*;
 
-macro_rules! recipe_test
+macro_rules! recipe_prep
 {
 	($name:expr) =>
 	({
+		std::sync::
+		let _ = remove_file(Path::new("recipe.txt"));
 		let mut f = File::open(Path::new($name)).unwrap();
 		let mut recf = File::create(Path::new("recipe.txt")).unwrap();
 		let mut contents: String = String::new();
 		let _ = f.read_to_string(&mut contents);
 		let _ = write!(recf, "{}", contents);
+	});
+}
+
+macro_rules! not_ok_test
+{
+	() =>
+	({
+		let mut rec: Recipe = Recipe::new();
+		rec.read_errors(true);
+		assert_eq!(rec.ok, false);
+		let _ = remove_file(Path::new("recipe.txt"));
+	});
+}
+
+macro_rules! ok_test
+{
+	() =>
+	({
+		let mut rec: Recipe = Recipe::new();
+		rec.read_errors(true);
+		assert_eq!(rec.ok, true);
+		let _ = remove_file(Path::new("recipe.txt"));
 	});
 }
 
@@ -32,7 +60,7 @@ fn find_success()
 #[test]
 fn basic_parse()
 {
-	recipe_test!("tests/basic_parse");
+	recipe_prep!("tests/basic_parse");
 
 	let mut rec: Recipe = Recipe::new();
 	rec.read_errors(true);
@@ -41,4 +69,18 @@ fn basic_parse()
 	assert_eq!(rec.targets[0].kind, TargetType::Executable);
 	assert_eq!(rec.targets[0].files.len(), 1);
 	let _ = remove_file(Path::new("recipe.txt"));
+}
+
+#[test]
+fn executable_start_extra_token()
+{
+	recipe_prep!("tests/executable_start_extra_token");
+	not_ok_test!();
+}
+
+#[test]
+fn library_start_extra_token()
+{
+	recipe_prep!("tests/library_start_extra_token");
+	not_ok_test!();
 }
